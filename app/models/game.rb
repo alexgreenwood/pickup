9a7d432017b077939b560e27b46242830d82c742
alex greenwood <alexander.greenwood@gmail.com>
2014-06-03 22:16:00 -0500

    added placeholder for invite list

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 
   belongs_to :organizer, :class_name => "User"
 
-  has_many :participants, :class_name => "User"
+  has_many :players, :class_name => "User"
 
   has_many :invites
 
