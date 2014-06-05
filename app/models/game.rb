08f9c3604fede7518ce3b9e528b3f73fec6860ef
alex greenwood <alexander.greenwood@gmail.com>
2014-06-04 19:15:56 -0500

    fixed players association
    list on game show page now reflects users who have rsvp’d

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 
   belongs_to :organizer, :class_name => "User"
 
-  has_many :players, :class_name => "User"
+  has_many :players, :through => :invites, :source => :user
 
   has_many :invites
 
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,6 +4,4 @@ belongs_to :user
 
 belongs_to :game
 
-has_many :participants, :class_name => "User"
-
 end
