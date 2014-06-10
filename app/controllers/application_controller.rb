8b9245820f12bfb49b1bf2a533a5a378dc7c0a2f
alex greenwood <alexander.greenwood@gmail.com>
2014-06-09 19:05:09 -0500

    added custom fields to devise forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,19 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :email
+
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :email
+  end
+
+
+
 end
